0e22a96eec993c87510eac664cf17110537886e7
ShinyuYang <1449404535@qq.com>
2022-11-09 23:13:32 +0800

    deploy

---
+++ precache-manifest.dfb3c5fe7da35b948f6e8edb0f1772af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fb5febab0e9bce5a75c",
+    "url": "/PPmorney-new-website/css/app.ea23cd07.css"
+  },
+  {
+    "revision": "161a3769a4d872f3d38b24eb1bb999aa",
+    "url": "/PPmorney-new-website/index.html"
+  },
+  {
+    "revision": "6fb5febab0e9bce5a75c",
+    "url": "/PPmorney-new-website/js/app.14d7ad1a.js"
+  },
+  {
+    "revision": "3faa07636eb4177ef714",
+    "url": "/PPmorney-new-website/js/chunk-vendors.aeb99fa3.js"
+  },
+  {
+    "revision": "6576646f5c9ff69f4a17734836f662b5",
+    "url": "/PPmorney-new-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/PPmorney-new-website/robots.txt"
+  }
+]);
